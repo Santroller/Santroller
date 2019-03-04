69bfe3f3ee58029f013c49ead03d418535b7eee1
Sanjay Govind <sanjay.govind9@gmail.com>
2019-03-05 00:50:48 +1300

    Remove define that broke xinput

--- src/shared/lufa/LUFAConfig.h
+++ src/shared/lufa/LUFAConfig.h
@@ -69,7 +69,7 @@
 		#define USE_FLASH_DESCRIPTORS
 //		#define USE_EEPROM_DESCRIPTORS
 //		#define NO_INTERNAL_SERIAL
-		#define FIXED_CONTROL_ENDPOINT_SIZE      8
+		// #define FIXED_CONTROL_ENDPOINT_SIZE      8
 //		#define DEVICE_STATE_AS_GPIOR            {Insert Value Here}
 		#define FIXED_NUM_CONFIGURATIONS         1
 //		#define CONTROL_ONLY_DEVICE

e3f117b8162bdda940b759018ca7bdd4f9411d1f
Sanjay Govind <govindsanj@ecs.vuw.ac.nz>
2019-03-21 14:32:13 +1300

    Clean up config, so that its easier to understand

--- src/config/config.h
+++ src/config/config.h
@@ -13,26 +13,26 @@
 #define DPAD 1
 #define JOY 2
 #if DEVICE_TYPE == DIRECT
-#define PIN_GREEN 4
-#define PIN_RED 5
-#define PIN_YELLOW 7
-#define PIN_BLUE 6
-#define PIN_ORANGE 8
-#define PIN_START 16
-#define PIN_SELECT 9
-#define PIN_WHAMMY 18
-#define PIN_UP 14
-#define PIN_DOWN 15
-#define PIN_LEFT 10
-#define PIN_RIGHT 21
-#define PIN_JOY_X 19
-#define PIN_JOY_Y 20
+#define PIN_GREEN_FRET 4
+#define PIN_RED_FRET 5
+#define PIN_YELLOW_FRET 7
+#define PIN_BLUE_FRET 6
+#define PIN_ORANGE_FRET 8
+#define PIN_START_BUTTON 16
+#define PIN_SELECT_BUTTON 9
+#define PIN_WHAMMY_POTENIOMETER 18
+#define PIN_STRUM_UP_BUTTON 14
+#define PIN_STRUM_DOWN_BUTTON 15
+#define PIN_DPAD_LEFT_BUTTON 10
+#define PIN_DPAD_RIGHT_BUTTON 21
+#define PIN_JOYSTICK_X_POTENIOMETER 19
+#define PIN_JOYSTICK_Y_POTENIOMETER 20
 #define DIRECTION_MODE JOY
 // Set this to JOY if your controller has a joystick, and set Joy X and Joy Y to
 // the X and Y for your joystick Set this to DPAD if your controller has a DPAD,
 // and set Left and Right to left and right on your dpad. Reuse pins for up and
 // down and strumming, if required.
-#define WHAMMY_DIRECTION -1
+#define INVERT_WHAMMY true
 // If your whammy bar appears inverted, set this to -1 to invert the output of your whammy bar.
 #define WHAMMY_INITIAL_VALUE 16863
 // Set this value to define where 0 is on your whammy bar.
@@ -44,11 +44,11 @@
 // For a list of keyboard bindings, visit
 // http://fourwalledcubicle.com/files/LUFA/Doc/151115/html/group___group___u_s_b_class_h_i_d_common.html
 #if OUTPUT_TYPE == KEYBOARD
-#define KEY_GREEN HID_KEYBOARD_SC_A
-#define KEY_RED HID_KEYBOARD_SC_S
-#define KEY_YELLOW HID_KEYBOARD_SC_J
-#define KEY_BLUE HID_KEYBOARD_SC_K
-#define KEY_ORANGE HID_KEYBOARD_SC_L
+#define KEY_GREEN_FRET HID_KEYBOARD_SC_A
+#define KEY_RED_FRET HID_KEYBOARD_SC_S
+#define KEY_YELLOW_FRET HID_KEYBOARD_SC_J
+#define KEY_BLUE_FRET HID_KEYBOARD_SC_K
+#define KEY_ORANGE_FRET HID_KEYBOARD_SC_L
 #define KEY_WHAMMY HID_KEYBOARD_SC_SEMICOLON_AND_COLON
 #define KEY_START HID_KEYBOARD_SC_ENTER
 #define KEY_SELECT HID_KEYBOARD_SC_H
--- src/shared/direct/Direct.cpp
+++ src/shared/direct/Direct.cpp
@@ -1,26 +1,39 @@
 #include "./Direct.h"
 void Direct::read_controller(Controller *controller) {
 #if DEVICE_TYPE == DIRECT
-  bit_write(IO::digitalRead(PIN_GREEN) == FRETS_LED, controller->buttons, GREEN);
-  bit_write(IO::digitalRead(PIN_RED) == FRETS_LED, controller->buttons, RED);
-  bit_write(IO::digitalRead(PIN_YELLOW) == FRETS_LED, controller->buttons, YELLOW);
-  bit_write(IO::digitalRead(PIN_BLUE) == FRETS_LED, controller->buttons, BLUE);
-  bit_write(IO::digitalRead(PIN_ORANGE) == FRETS_LED, controller->buttons, ORANGE);
-  bit_write(!IO::digitalRead(PIN_START), controller->buttons, START);
-  bit_write(!IO::digitalRead(PIN_SELECT), controller->buttons, SELECT);
-  controller->r_x = (IO::analogRead(PIN_WHAMMY) * 32) * WHAMMY_DIRECTION + WHAMMY_INITIAL_VALUE;
+  bit_write(IO::digitalRead(PIN_GREEN_FRET) == FRETS_LED, controller->buttons,
+            GREEN);
+  bit_write(IO::digitalRead(PIN_RED_FRET) == FRETS_LED, controller->buttons,
+            RED);
+  bit_write(IO::digitalRead(PIN_YELLOW_FRET) == FRETS_LED, controller->buttons,
+            YELLOW);
+  bit_write(IO::digitalRead(PIN_BLUE_FRET) == FRETS_LED, controller->buttons,
+            BLUE);
+  bit_write(IO::digitalRead(PIN_ORANGE_FRET) == FRETS_LED, controller->buttons,
+            ORANGE);
+  bit_write(!IO::digitalRead(PIN_START_BUTTON), controller->buttons, START);
+  bit_write(!IO::digitalRead(PIN_SELECT_BUTTON), controller->buttons, SELECT);
+  controller->r_x =
+      (IO::analogRead(PIN_WHAMMY_POTENIOMETER) * 32) * INVERT_WHAMMY
+          ? -1
+          : 1 + WHAMMY_INITIAL_VALUE;
 #if DIRECTION_MODE == JOY
-  bit_write(!IO::digitalRead(PIN_UP) || IO::analogRead(PIN_JOY_Y) > 600,
+  bit_write(!IO::digitalRead(PIN_STRUM_UP_BUTTON) ||
+                IO::analogRead(PIN_JOYSTICK_Y_POTENIOMETER) > 600,
             controller->buttons, UP);
-  bit_write(!IO::digitalRead(PIN_DOWN) || IO::analogRead(PIN_JOY_Y) < 400,
+  bit_write(!IO::digitalRead(PIN_STRUM_DOWN_BUTTON) ||
+                IO::analogRead(PIN_JOYSTICK_Y_POTENIOMETER) < 400,
             controller->buttons, DOWN);
-  bit_write(IO::analogRead(PIN_JOY_X) < 400, controller->buttons, LEFT);
-  bit_write(IO::analogRead(PIN_JOY_X) > 600, controller->buttons, RIGHT);
+  bit_write(IO::analogRead(PIN_JOYSTICK_X_POTENIOMETER) < 400,
+            controller->buttons, LEFT);
+  bit_write(IO::analogRead(PIN_JOYSTICK_X_POTENIOMETER) > 600,
+            controller->buttons, RIGHT);
 #elif DIRECTION_MODE == DPAD
-  bit_write(!IO::digitalRead(PIN_UP), controller->buttons, UP);
-  bit_write(!IO::digitalRead(PIN_DOWN), controller->buttons, DOWN);
-  bit_write(!IO::digitalRead(PIN_LEFT), controller->buttons, LEFT);
-  bit_write(!IO::digitalRead(PIN_RIGHT), controller->buttons, RIGHT);
+  bit_write(!IO::digitalRead(PIN_STRUM_UP_BUTTON), controller->buttons, UP);
+  bit_write(!IO::digitalRead(PIN_STRUM_DOWN_BUTTON), controller->buttons, DOWN);
+  bit_write(!IO::digitalRead(PIN_DPAD_LEFT_BUTTON), controller->buttons, LEFT);
+  bit_write(!IO::digitalRead(PIN_DPAD_RIGHT_BUTTON), controller->buttons,
+            RIGHT);
 #endif
 #endif
 }
@@ -28,25 +41,25 @@ void Direct::read_controller(Controller *controller) {
 void Direct::init() {
 #if DEVICE_TYPE == DIRECT
   int fret_type = INPUT_PULLUP;
-  #if FRETS_LED == 1 
-    fret_type = INPUT;
-  #endif
-  IO::pinMode(PIN_GREEN, fret_type);
-  IO::pinMode(PIN_RED, fret_type);
-  IO::pinMode(PIN_YELLOW, fret_type);
-  IO::pinMode(PIN_BLUE, fret_type);
-  IO::pinMode(PIN_ORANGE, fret_type);
-  IO::pinMode(PIN_START, INPUT_PULLUP);
-  IO::pinMode(PIN_SELECT, INPUT_PULLUP);
-  IO::pinMode(PIN_UP, INPUT_PULLUP);
-  IO::pinMode(PIN_DOWN, INPUT_PULLUP);
-  IO::pinMode(PIN_WHAMMY, INPUT);
+#if FRETS_LED == 1
+  fret_type = INPUT;
+#endif
+  IO::pinMode(PIN_GREEN_FRET, fret_type);
+  IO::pinMode(PIN_RED_FRET, fret_type);
+  IO::pinMode(PIN_YELLOW_FRET, fret_type);
+  IO::pinMode(PIN_BLUE_FRET, fret_type);
+  IO::pinMode(PIN_ORANGE_FRET, fret_type);
+  IO::pinMode(PIN_START_BUTTON, INPUT_PULLUP);
+  IO::pinMode(PIN_SELECT_BUTTON, INPUT_PULLUP);
+  IO::pinMode(PIN_STRUM_UP_BUTTON, INPUT_PULLUP);
+  IO::pinMode(PIN_STRUM_DOWN_BUTTON, INPUT_PULLUP);
+  IO::pinMode(PIN_WHAMMY_POTENIOMETER, INPUT);
 #if DIRECTION_MODE == JOY
-  IO::pinMode(PIN_JOY_X, INPUT);
-  IO::pinMode(PIN_JOY_Y, INPUT);
+  IO::pinMode(PIN_JOYSTICK_X_POTENIOMETER, INPUT);
+  IO::pinMode(PIN_JOYSTICK_Y_POTENIOMETER, INPUT);
 #elif DIRECTION_MODE == DPAD
-  IO::pinMode(PIN_LEFT, INPUT_PULLUP);
-  IO::pinMode(PIN_RIGHT, INPUT_PULLUP);
+  IO::pinMode(PIN_DPAD_LEFT_BUTTON, INPUT_PULLUP);
+  IO::pinMode(PIN_DPAD_RIGHT_BUTTON, INPUT_PULLUP);
 #endif
 #endif
 }

2ab323f9dd2fb9698c61cf03b9c381eacd5cb7c9
Sanjay Govind <govindsanj@ecs.vuw.ac.nz>
2019-04-22 22:25:21 +1200

    Remove unnecessary extern c

--- src/shared/controller/output/OutputHandler.cpp
+++ src/shared/controller/output/OutputHandler.cpp
@@ -8,23 +8,27 @@ void OutputHandler::process(Controller *controller) {
   output.update(*controller);
 }
 
-void OutputHandler::init() {
-  output.init();
+void OutputHandler::init() { output.init(); }
+
+bool OutputHandler::ready() { return output.ready(); }
+
+uint16_t get_descriptor(const uint8_t DescriptorType,
+                        const uint8_t DescriptorNumber,
+                        const void **const DescriptorAddress) {
+  return OutputHandler::output.get_descriptor(DescriptorType, DescriptorNumber,
+                                              DescriptorAddress);
 }
 
-bool OutputHandler::ready() {
-  return output.ready();
-}
-extern "C" {
 void EVENT_USB_Device_Connect(void) { OutputHandler::output.usb_connect(); }
-void EVENT_USB_Device_Disconnect(void) { OutputHandler::output.usb_disconnect(); }
+void EVENT_USB_Device_Disconnect(void) {
+  OutputHandler::output.usb_disconnect();
+}
 void EVENT_USB_Device_ConfigurationChanged(void) {
   OutputHandler::output.usb_configuration_changed();
 }
-void EVENT_USB_Device_ControlRequest(void) { OutputHandler::output.usb_control_request(); }
-void EVENT_USB_Device_StartOfFrame(void) { OutputHandler::output.usb_start_of_frame(); }
+void EVENT_USB_Device_ControlRequest(void) {
+  OutputHandler::output.usb_control_request();
 }
-
-uint16_t get_descriptor(const uint8_t DescriptorType, const uint8_t DescriptorNumber, const void **const DescriptorAddress) {
-  return OutputHandler::output.get_descriptor(DescriptorType, DescriptorNumber, DescriptorAddress);
+void EVENT_USB_Device_StartOfFrame(void) {
+  OutputHandler::output.usb_start_of_frame();
 }

73e4b74db0c91cf07603a504ca43a41e424c398b
Sanjay Govind <govindsanj@ecs.vuw.ac.nz>
2019-04-29 18:13:24 +1200

    Use SerialOutput as a way to poll data from the avr

--- src/micro/main.cpp
+++ src/micro/main.cpp
@@ -1,11 +1,13 @@
 #include "../shared/controller/input/InputHandler.h"
 #include "../shared/controller/output/OutputHandler.h"
 #include "pins.h"
-
+#include "../shared/sharedmain.h"
 InputHandler controller;
 OutputHandler out;
+Main main2;
 
 int main() {
+  main2.main();
   out.init();
   controller.init();
   while (true) {

8e3bf6d95ba76a5a6144323cde84a771f426f444
Sanjay Govind <govindsanj@ecs.vuw.ac.nz>
2019-07-08 21:50:48 +1200

    Add other controller ids to upload.py

--- upload.py
+++ upload.py
@@ -2,15 +2,32 @@
 
 import usb.core
 import sys
-#Make this also handle ps3 guitars
+
+
+class find_class(object):
+    def __call__(self, device):
+        # first, let's check the device
+        if device.idVendor == 0x1209 and device.idProduct == 0x2882:
+            return True
+        if device.idVendor == 0x0F0D and device.idProduct == 0x0092:
+            return True
+        if device.idVendor == 0x12ba and device.idProduct == 0x0100:
+            return True
+        if device.idVendor == 0x12ba and device.idProduct == 0x0200:
+            return True
+        if device.idVendor == 0x12ba and device.idProduct == 0x0120:
+            return True
+        if device.idVendor == 0x12ba and device.idProduct == 0x0210:
+            return True
+        return False
+
+
+# Make this also handle ps3 guitars
 try:
-    dev = usb.core.find(idVendor=0x1209, idProduct=0x2882)
-    val = 0x30
-    if len(sys.argv) > 1 and sys.argv[1] == "serial":
-        val = 0x31
+    dev = usb.core.find(custom_match=find_class())
     if dev is None:
         raise ValueError('Device not found')
     else:
-            dev.ctrl_transfer(0x00, val, 0x00, 0x00)
+        dev.ctrl_transfer(0x00, 0x30, 0x00, 0x00)
 except:
     pass

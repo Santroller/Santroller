feb8320231bc137f2efc693a8de8cfcfa8b4585f
Sanjay Govind <govindsanj@ecs.vuw.ac.nz>
2019-08-12 16:38:50 +1200

    Add more sane defaults

--- src/shared/config/defaults.h
+++ src/shared/config/defaults.h
@@ -1,9 +1,9 @@
 #pragma once
 #include "./defines.h"
 #define PROTOCOL_VERSION 0
-#define TILT_SENSOR ANALOGUE
-#define DEVICE_TYPE WII
-#define OUTPUT_TYPE XINPUT_DRUM_SUBTYPE
+#define TILT_SENSOR NONE
+#define DEVICE_TYPE DIRECT
+#define OUTPUT_TYPE XINPUT_GAMEPAD_SUBTYPE
 #define POLL_RATE 1
 // Thresholds
 #define TRIGGER_THRESHOLD 12767

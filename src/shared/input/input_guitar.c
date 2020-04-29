273985c6823ec61dd8789170f058ad9ada93667e
Sanjay Govind <sanjay.govind9@gmail.com>
2020-04-29 19:47:20 +1200

    Fix xinput on windows! (also have a seperation between rb and gh

--- src/shared/input/input_guitar.c
+++ src/shared/input/input_guitar.c
@@ -28,7 +28,7 @@ void digital_tick(controller_t *controller) {
 void (*tick)(controller_t *controller) = NULL;
 
 bool is_drum(void) {
-  return config.main.input_type == PS3_GUITAR_HERO_DRUMS || PS3_ROCK_BAND_DRUMS || WII_ROCK_BAND_DRUMS || XINPUT_DRUMS;
+  return config.main.input_type == PS3_GUITAR_HERO_DRUMS || PS3_ROCK_BAND_DRUMS || WII_ROCK_BAND_DRUMS || XINPUT_GUITAR_HERO_DRUMS || XINPUT_ROCK_BAND_DRUMS;
 }
 bool is_not_guitar(void) {
   return config.main.sub_type != PS3_GUITAR_HERO_GUITAR &&

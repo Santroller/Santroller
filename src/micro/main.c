1b630d30bf3982934f79c2c7f59a34d1c3ffc6eb
Sanjay Govind <sanjay.govind9@gmail.com>
2020-05-09 16:02:39 +1200

    Remove hardcoding

--- src/micro/main.c
+++ src/micro/main.c
@@ -73,7 +73,6 @@ output_report_size_t report;
 int main(void) {
   load_config();
   init_serial();
-  config.main.sub_type = MIDI_GUITAR;
   device_type = config.main.sub_type;
   input_init();
   report_init();

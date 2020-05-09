df3bb1361dff7ec682390c9063e6acf9d47d3418
Sanjay Govind <sanjay.govind9@gmail.com>
2020-05-09 22:03:49 +1200

    Fix selecting led colours, migrate buttons to enums

--- src/shared/controller/controller.h
+++ src/shared/controller/controller.h
@@ -1,43 +1,7 @@
 #pragma once
+#include "controller_defines.h"
 #include <stdint.h>
-enum xbox_buttons {
-  XBOX_DPAD_UP,
-  XBOX_DPAD_DOWN,
-  XBOX_DPAD_LEFT,
-  XBOX_DPAD_RIGHT,
-  XBOX_START,
-  XBOX_BACK,
-  XBOX_LEFT_STICK,
-  XBOX_RIGHT_STICK,
 
-  XBOX_LB,
-  XBOX_RB,
-  XBOX_HOME,
-  XBOX_UNUSED,
-  XBOX_A,
-  XBOX_B,
-  XBOX_X,
-  XBOX_Y,
-};
-#define XBOX_BTN_COUNT 16
-#define XBOX_AXIS_COUNT 6
-
-enum switch_buttons {
-  SWITCH_Y,
-  SWITCH_B,
-  SWITCH_A,
-  SWITCH_X,
-  SWITCH_L,
-  SWITCH_R,
-  SWITCH_ZL,
-  SWITCH_ZR,
-  SWITCH_SELECT,
-  SWITCH_START,
-  SWITCH_LEFT_STICK,
-  SWITCH_RIGHT_STICK,
-  SWITCH_HOME,
-  SWITCH_CAPTURE,
-};
 typedef struct {
   uint8_t leds[5];
   uint32_t gui;

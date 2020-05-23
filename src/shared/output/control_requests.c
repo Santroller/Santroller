761095adcfc5321f9de59bacf8e59365ee346752
Sanjay Govind <sanjay.govind9@gmail.com>
2020-05-23 22:43:37 +1200

    Fix uno again

--- src/shared/output/control_requests.c
+++ src/shared/output/control_requests.c
@@ -30,7 +30,7 @@ void deviceControlRequest(void) {
     Endpoint_ClearStatusStage();
     return;
   }
-#ifndef __AVR_ATmega16U2__
+#ifdef __AVR_ATmega32U4__
   if (deviceType < PS3_GAMEPAD || USB_ControlRequest.wIndex != INTERFACE_ID_HID)
     return;
 #endif

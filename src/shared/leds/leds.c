ae3c7f45c4e218006e81968d5db7b7bc20888667
Sanjay Govind <sanjay.govind9@gmail.com>
2020-07-21 21:20:59 +1200

    Revert leds from being interrupt based to being polling based

--- src/shared/leds/leds.c
+++ src/shared/leds/leds.c
@@ -17,9 +17,6 @@ void initLEDs(void) {
   // set as master
   SPCR |= 1 << MSTR;
 
-  // enable interrupts
-  SPCR |= 1 << SPIE;
-
   // set clock polarity/phase to mode 3
   SPCR |= (1 << CPOL) | (1 << CPHA);
 
@@ -27,18 +24,17 @@ void initLEDs(void) {
   SPSR |= 1 << SPI2X;
   SPCR |= (1 << SPR1) | (1 << SPR0);
 }
-volatile int currentIndex = -1;
-volatile int maxIndex = 0;
-uint8_t ledData[((XBOX_BTN_COUNT + XBOX_AXIS_COUNT) + 1) * 4] = {0};
+/* Send out data via SPI & wait until transmission is complete */
+void transmitSPIByte(uint8_t data) {
+  SPDR = data;
+  while (!(SPSR & _BV(SPIF)))
+    ;
+}
 void tickLEDs(Controller_t *controller) {
-  // Don't do anything if we are in the middle of writing to the leds at the current moment. Or if the leds are disabled.
-  if (config.main.fretLEDMode != APA102 ||
-      (currentIndex >= 0 && currentIndex < maxIndex))
-    return;
-  currentIndex = 0;
+  // Don't do anything if the leds are disabled.
+  if (config.main.fretLEDMode != APA102) return;
   int led = 0;
-  // We need to transmit 4 zeros as start bytes, and they are already in ledData, so we just skip to byte 5.
-  int idx = 4;
+  for (uint8_t i = 0; i < 4; i++) { transmitSPIByte(0); }
   Led_t configLED;
   Led_t contLED;
   // Loop until either config.leds runs out, or controller->leds runs out. This
@@ -54,20 +50,13 @@ void tickLEDs(Controller_t *controller) {
       if (getVelocity(controller, configLED.pin-1)) { contLED = configLED; }
     }
     // Write an leds colours
-    ledData[idx++] = 0xff;
-    ledData[idx++] = contLED.blue;
-    ledData[idx++] = contLED.green;
-    ledData[idx++] = contLED.red;
+    transmitSPIByte(0xff);
+    transmitSPIByte(contLED.blue);
+    transmitSPIByte(contLED.green);
+    transmitSPIByte(contLED.red);
     led++;
   }
   // We need to send the correct amount of stop bytes
   uint8_t stop_bytes = (led + 15) / 16;
-  for (uint8_t i = 0; i < stop_bytes; i++) { ledData[idx++] = 0xff; }
-  maxIndex = idx;
-  // A packet has been created, we need to send the first byte to start transmission.
-  SPDR = ledData[currentIndex++];
-}
-ISR(SPI_STC_vect) {
-  if (currentIndex == -1 || currentIndex == maxIndex) return;
-  SPDR = ledData[currentIndex++];
+  for (uint8_t i = 0; i < stop_bytes; i++) { transmitSPIByte(0xff); }
 }

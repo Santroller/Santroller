c3b1e434756e4abdb205bf2f06595fd973824de2
Sanjay Govind <sanjay.govind9@gmail.com>
2021-02-08 11:37:51 +1300

    Flickering fix!

--- src/shared/output/control_requests.c
+++ src/shared/output/control_requests.c
@@ -8,6 +8,19 @@
 #else
 #  define CompatibleDescriptorType USB_OSCompatibleIDDescriptor_t
 #endif
+// Dumps from a real guitar
+// const PROGMEM uint8_t c0w0000b1[] = {0x00, 0x82, 0xf8, 0x23};
+const PROGMEM uint8_t c1w0000b1[] = {0x00, 0x08, 0x00, 0x00,
+                                     0x00, 0x00, 0x00, 0x00};
+// const PROGMEM uint8_t c1w0100b1[] = {0x00, 0x14, 0x3f, 0xf7, 0xff, 0xff, 0x00,
+//                                      0x00, 0x00, 0x00, 0xc0, 0xff, 0xc0, 0xff,
+//                                      0x00, 0x00, 0x00, 0x00, 0x00, 0x00};
+
+typedef struct {
+  uint8_t bReportId;
+  uint8_t bLength;
+  uint8_t data[6];
+} XIDGamepadCapabilities;
 const PROGMEM USB_OSExtendedCompatibleIDDescriptor_t ExtendedIDs = {
   TotalLength : sizeof(USB_OSExtendedCompatibleIDDescriptor_t),
   Version : 0x0100,
@@ -70,9 +83,10 @@ const PROGMEM CompatibleDescriptorType DevCompatIDs = {
   }
 #endif
 };
-
 void deviceControlRequest(void) {
   if (!(Endpoint_IsSETUPReceived())) return;
+  const void *buffer = NULL;
+  uint8_t len = 0;
   if (USB_ControlRequest.bRequest == HID_REQ_GetReport &&
       USB_ControlRequest.bmRequestType ==
           (REQDIR_DEVICETOHOST | REQTYPE_CLASS | REQREC_INTERFACE)) {
@@ -80,24 +94,46 @@ void deviceControlRequest(void) {
   } else if (USB_ControlRequest.bRequest == HID_REQ_SetReport &&
              USB_ControlRequest.bmRequestType ==
                  (REQDIR_HOSTTODEVICE | REQTYPE_CLASS | REQREC_INTERFACE)) {
-    processHIDWriteFeatureReportControl(USB_ControlRequest.wValue, USB_ControlRequest.wLength);
+    processHIDWriteFeatureReportControl(USB_ControlRequest.wValue,
+                                        USB_ControlRequest.wLength);
+  } else if (USB_ControlRequest.bRequest == 0x1 &&
+             (USB_ControlRequest.bmRequestType == 0xC1) &&
+             USB_ControlRequest.wIndex == INTERFACE_ID_XInput &&
+             USB_ControlRequest.wValue == 0x0000) {
+    len = sizeof(c1w0000b1);
+    buffer = &c1w0000b1;
   } else if (USB_ControlRequest.bRequest == REQ_GetOSFeatureDescriptor &&
              (USB_ControlRequest.bmRequestType == 0xC1) &&
              USB_ControlRequest.wIndex == EXTENDED_PROPERTIES_DESCRIPTOR &&
              USB_ControlRequest.wValue == INTERFACE_ID_Config) {
-    Endpoint_ClearSETUP();
-    Endpoint_Write_Control_PStream_LE(&ExtendedIDs, ExtendedIDs.TotalLength);
-    Endpoint_ClearStatusStage();
+    len = ExtendedIDs.TotalLength;
+    buffer = &ExtendedIDs;
   } else if (USB_ControlRequest.bRequest == REQ_GetOSFeatureDescriptor &&
              USB_ControlRequest.bmRequestType ==
                  (REQDIR_DEVICETOHOST | REQTYPE_VENDOR) &&
              USB_ControlRequest.wIndex == EXTENDED_COMPAT_ID_DESCRIPTOR) {
-    Endpoint_ClearSETUP();
-    Endpoint_Write_Control_PStream_LE(&DevCompatIDs, DevCompatIDs.TotalLength);
-    Endpoint_ClearStatusStage();
+    len = DevCompatIDs.TotalLength;
+    buffer = &DevCompatIDs;
   } else if (USB_ControlRequest.bmRequestType ==
              (REQDIR_HOSTTODEVICE | REQTYPE_CLASS | REQREC_INTERFACE)) {
+    buffer = &DevCompatIDs;
+  }
+  // else if (USB_ControlRequest.bRequest == 0x1 &&
+  //            (USB_ControlRequest.bmRequestType == 0xC0) &&
+  //            USB_ControlRequest.wIndex == 0x00 &&
+  //            USB_ControlRequest.wValue == 0x0000) {
+  //   len = sizeof(c1w0000b1);
+  //   buffer = &c0w0000b1;
+  // } else if (USB_ControlRequest.bRequest == 0x1 &&
+  //            (USB_ControlRequest.bmRequestType == 0xC1) &&
+  //            USB_ControlRequest.wIndex == INTERFACE_ID_XInput &&
+  //            USB_ControlRequest.wValue == 0x0100) {
+  //   len = sizeof(c1w0100b1);
+  //   buffer = &c1w0100b1;
+  // }
+  if (buffer) {
     Endpoint_ClearSETUP();
+    Endpoint_Write_Control_PStream_LE(buffer, len);
     Endpoint_ClearStatusStage();
   }
 }

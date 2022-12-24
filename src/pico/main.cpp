2b7be8959b04e1089fc4d02a3517b1e7e3ada445
Sanjay Govind <sanjay.govind0@gmail.com>
2022-12-24 13:04:22 +1300

    Infrequently poll on the picos even if HID isnt waiting, so that the config tool gets inputs

--- src/pico/main.cpp
+++ src/pico/main.cpp
@@ -28,7 +28,8 @@ uint16_t host_pid = 0;
 uint8_t host_dev_addr = 0;
 bool passthrough_ready = false;
 
-void setup() {
+void setup()
+{
     uart_set_baudrate(uart0, 115200);
     generateSerialString(serialString.UnicodeString);
     tusb_init();
@@ -36,8 +37,15 @@ void setup() {
 }
 bool reset_on_next = false;
 USB_Report_Data_t report;
-void loop() {
-    if (reset_on_next) {
+long last = 0;
+#define TICK_CHECK
+#if CONSOLE_TYPE != MIDI
+#define TICK_CHECK tud_xinput_n_ready(0)
+#endif
+void loop()
+{
+    if (reset_on_next)
+    {
         tud_disconnect();
         sleep_ms(100);
         tud_connect();
@@ -46,30 +54,43 @@ void loop() {
     }
     tud_task();
     tuh_task();
-    if (consoleType == MIDI) {
-        // uint8_t size = tick(&report);
+    uint8_t size = 0;
+    if (TICK_CHECK || last - millis() > 5)
+    {
+        last = millis();
+        size = tick(&report);
+    }
+
+    if (size)
+    {
+#if CONSOLE_TYPE == MIDI
         // tud_midi_n_packet_write(0, report);
-    } else {
-        if (tud_xinput_n_ready(0)) {
-            uint8_t size2 = tick(&report);
-            tud_xinput_n_report(0, 0, &report, size2);
+#else
+        if (tud_xinput_n_ready(0))
+        {
+            tud_xinput_n_report(0, 0, &report, size);
         }
+#endif
     }
 }
 
-void tuh_xinput_mount_cb(uint8_t dev_addr, uint8_t instance) {
-    if (passthrough_ready) {
+void tuh_xinput_mount_cb(uint8_t dev_addr, uint8_t instance)
+{
+    if (passthrough_ready)
+    {
         return;
     }
     host_dev_addr = dev_addr;
     tuh_vid_pid_get(dev_addr, &host_vid, &host_pid);
     passthrough_ready = true;
-    if (consoleType == XBOX360) {
+    if (consoleType == XBOX360)
+    {
         reset_usb();
     }
 }
 
-void tuh_xinput_umount_cb(uint8_t dev_addr, uint8_t instance) {
+void tuh_xinput_umount_cb(uint8_t dev_addr, uint8_t instance)
+{
     host_vid = 0;
     host_pid = 0;
     host_dev_addr = 0;
@@ -77,72 +98,96 @@ void tuh_xinput_umount_cb(uint8_t dev_addr, uint8_t instance) {
     reset_usb();
 }
 
-uint8_t const *tud_descriptor_device_cb(void) {
+uint8_t const *tud_descriptor_device_cb(void)
+{
     descriptorRequest(USB_DESCRIPTOR_DEVICE << 8, 0, buf);
-    if (passthrough_ready && consoleType == XBOX360) {
+    if (passthrough_ready && consoleType == XBOX360)
+    {
         USB_DEVICE_DESCRIPTOR *td = (USB_DEVICE_DESCRIPTOR *)buf;
         td->idVendor = host_vid;
         td->idProduct = host_pid;
     }
     return buf;
 }
-uint8_t const *tud_hid_custom_descriptor_report_cb(uint8_t instance) {
+uint8_t const *tud_hid_custom_descriptor_report_cb(uint8_t instance)
+{
     descriptorRequest(HID_DESCRIPTOR_REPORT << 8, 0, buf);
     return buf;
 }
-uint8_t const *tud_descriptor_configuration_cb(uint8_t index) {
-    (void)index;  // for multiple configurations
+uint8_t const *tud_descriptor_configuration_cb(uint8_t index)
+{
+    (void)index; // for multiple configurations
     descriptorRequest(USB_DESCRIPTOR_CONFIGURATION << 8, 0, buf);
     return buf;
 }
-uint16_t const *tud_descriptor_string_cb(uint8_t index, uint16_t langid) {
+uint16_t const *tud_descriptor_string_cb(uint8_t index, uint16_t langid)
+{
     // Arduinos handle their own serial descriptor, so theres no point in sharing an implementation there.
-    if (index == 3) {
+    if (index == 3)
+    {
         return (uint16_t *)&serialString;
     }
-    if (descriptorRequest(USB_DESCRIPTOR_STRING << 8 | index, 0, buf)) {
+    if (descriptorRequest(USB_DESCRIPTOR_STRING << 8 | index, 0, buf))
+    {
         return (uint16_t *)buf;
     }
     return NULL;
 }
 
-void reset_usb(void) {
+void reset_usb(void)
+{
     reset_on_next = true;
 }
 tusb_control_request_t lastreq;
-bool tud_vendor_control_xfer_cb(uint8_t rhport, uint8_t stage, tusb_control_request_t const *request) {
-    if (request->bmRequestType_bit.type == TUSB_REQ_TYPE_STANDARD && request->bRequest == TUSB_REQ_GET_DESCRIPTOR) {
+bool tud_vendor_control_xfer_cb(uint8_t rhport, uint8_t stage, tusb_control_request_t const *request)
+{
+    if (request->bmRequestType_bit.type == TUSB_REQ_TYPE_STANDARD && request->bRequest == TUSB_REQ_GET_DESCRIPTOR)
+    {
         //------------- STD Request -------------//
-        if (stage == CONTROL_STAGE_SETUP) {
+        if (stage == CONTROL_STAGE_SETUP)
+        {
             uint8_t const desc_type = tu_u16_high(request->wValue);
             // uint8_t const desc_index = tu_u16_low (request->wValue);
 
-            if (request->bRequest == TUSB_REQ_GET_DESCRIPTOR && desc_type == HID_DESCRIPTOR_HID) {
+            if (request->bRequest == TUSB_REQ_GET_DESCRIPTOR && desc_type == HID_DESCRIPTOR_HID)
+            {
                 uint16_t len = descriptorRequest(HID_DESCRIPTOR_HID << 8, 0, buf);
                 TU_VERIFY(tud_control_xfer(rhport, request, buf, len));
                 return true;
-            } else if (request->bRequest == TUSB_REQ_GET_DESCRIPTOR && desc_type == HID_DESCRIPTOR_REPORT) {
+            }
+            else if (request->bRequest == TUSB_REQ_GET_DESCRIPTOR && desc_type == HID_DESCRIPTOR_REPORT)
+            {
                 uint16_t len = descriptorRequest(HID_DESCRIPTOR_REPORT << 8, 0, buf);
                 TU_VERIFY(tud_control_xfer(rhport, request, buf, len));
                 return true;
-            } else {
-                return false;  // stall unsupported request
+            }
+            else
+            {
+                return false; // stall unsupported request
             }
         }
     }
-    if (controlRequestValid(request->bmRequestType, request->bRequest, request->wValue, request->wIndex, request->wLength)) {
-        if (request->bmRequestType_bit.direction == TUSB_DIR_IN) {
-            if (stage == CONTROL_STAGE_SETUP) {
+    if (controlRequestValid(request->bmRequestType, request->bRequest, request->wValue, request->wIndex, request->wLength))
+    {
+        if (request->bmRequestType_bit.direction == TUSB_DIR_IN)
+        {
+            if (stage == CONTROL_STAGE_SETUP)
+            {
                 uint16_t len = controlRequest(request->bmRequestType, request->bRequest, request->wValue, request->wIndex, request->wLength, buf);
                 tud_control_xfer(rhport, request, buf, len);
             }
-        } else if (stage == CONTROL_STAGE_SETUP) {
+        }
+        else if (stage == CONTROL_STAGE_SETUP)
+        {
             tud_control_xfer(rhport, request, buf, request->wLength);
         }
-        if (stage == CONTROL_STAGE_DATA || (stage == CONTROL_STAGE_SETUP && !request->wLength)) {
+        if (stage == CONTROL_STAGE_DATA || (stage == CONTROL_STAGE_SETUP && !request->wLength))
+        {
             controlRequest(request->bmRequestType, request->bRequest, request->wValue, request->wIndex, request->wLength, buf);
         }
-    } else if (passthrough_ready) {
+    }
+    else if (passthrough_ready)
+    {
         tuh_xfer_t xfer = {};
         xfer.daddr = host_dev_addr;
         xfer.ep_addr = 0;
@@ -150,21 +195,29 @@ bool tud_vendor_control_xfer_cb(uint8_t rhport, uint8_t stage, tusb_control_requ
         xfer.buffer = buf;
         xfer.complete_cb = NULL;
         xfer.user_data = 0;
-        if (request->bmRequestType_bit.direction == TUSB_DIR_IN) {
-            if (stage == CONTROL_STAGE_SETUP) {
+        if (request->bmRequestType_bit.direction == TUSB_DIR_IN)
+        {
+            if (stage == CONTROL_STAGE_SETUP)
+            {
                 tuh_control_xfer(&xfer);
-                if (xfer.result != XFER_RESULT_SUCCESS) {
+                if (xfer.result != XFER_RESULT_SUCCESS)
+                {
                     return false;
                 }
                 tud_control_xfer(rhport, request, buf, request->wLength);
             }
-        } else {
-            if (stage == CONTROL_STAGE_SETUP) {
+        }
+        else
+        {
+            if (stage == CONTROL_STAGE_SETUP)
+            {
                 tud_control_xfer(rhport, request, buf, request->wLength);
             }
-            if (stage == CONTROL_STAGE_DATA || (stage == CONTROL_STAGE_SETUP && !request->wLength)) {
+            if (stage == CONTROL_STAGE_DATA || (stage == CONTROL_STAGE_SETUP && !request->wLength))
+            {
                 tuh_control_xfer(&xfer);
-                if (xfer.result != XFER_RESULT_SUCCESS) {
+                if (xfer.result != XFER_RESULT_SUCCESS)
+                {
                     return false;
                 }
             }
@@ -186,7 +239,8 @@ usbd_class_driver_t driver[] = {
         .xfer_cb = xinputd_xfer_cb,
         .sof = NULL}};
 
-usbd_class_driver_t const *usbd_app_driver_get_cb(uint8_t *driver_count) {
+usbd_class_driver_t const *usbd_app_driver_get_cb(uint8_t *driver_count)
+{
     *driver_count = 1;
     return driver;
 }
@@ -201,15 +255,19 @@ usbh_class_driver_t driver_host[] = {
         .xfer_cb = xinputh_xfer_cb,
         .close = xinputh_close}};
 
-usbh_class_driver_t const *usbh_app_driver_get_cb(uint8_t *driver_count) {
+usbh_class_driver_t const *usbh_app_driver_get_cb(uint8_t *driver_count)
+{
     *driver_count = 1;
     return driver_host;
 }
-void reboot(void) {
+void reboot(void)
+{
     watchdog_enable(1, false);
-    for (;;) {
+    for (;;)
+    {
     }
 }
-void bootloader(void) {
+void bootloader(void)
+{
     reset_usb_boot(0, 0);
 }

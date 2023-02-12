8ae829c2e58f74f848c4f68cb0214c8f623f651f
Sanjay Govind <sanjay.govind9@gmail.com>
2023-02-12 16:56:32 +1300

    Add HID to pico ble firmware

--- src/pico/pico_ble/main.cpp
+++ src/pico/pico_ble/main.cpp
@@ -1,17 +1,27 @@
 #include <Arduino.h>
 #include <SPI.h>
+#include <pico/bootrom.h>
 #include <pico/stdlib.h>
 #include <pico/unique_id.h>
 #include <string.h>
-#include <pico/bootrom.h>
 
+#include "Adafruit_TinyUSB.h"
 #include "commands.h"
 #include "shared_main.h"
-#include "btstack.h"
 
+uint8_t const desc_hid_report[] =
+    { TUD_HID_REPORT_DESC_GAMEPAD()};
+Adafruit_USBD_HID usb_hid(desc_hid_report, sizeof(desc_hid_report), HID_ITF_PROTOCOL_NONE, 2, false);
+hid_gamepad_report_t gp;
 
 void setup() {
     init_main();
+    Serial.begin(115200);
+    usb_hid.setPollInterval(1);
+    usb_hid.begin();
+    while (!TinyUSBDevice.mounted()) delay(1);
+
+    Serial.println("Adafruit TinyUSB HID Gamepad example");
 }
 USB_Report_Data_t report;
 unsigned int last = 0;
@@ -30,5 +40,4 @@ void bootloader(void) {
 void reset_usb(void) {
 }
 void send_report_to_controller(uint8_t *report, uint8_t len) {
-
 }

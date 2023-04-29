ab18050a8476e04bcf1b65e83fbe7adb6a1fa0a2
Sanjay Govind <sanjay.govind9@gmail.com>
2023-04-29 22:49:09 +1200

    Add delay

--- ardwiino_script_pre.py
+++ ardwiino_script_pre.py
@@ -65,6 +65,7 @@ if "upload" in BUILD_TARGETS:
         while not dev:
             dev = libusb_package.find(idVendor=0x1209, idProduct=0x2886)
             pass
+        sleep(2)
         rate = dev.product.split('\x00',1)[0].split(" - ")[2]
         rate = f"{rate}L"
         env["BOARD_F_CPU"] = rate

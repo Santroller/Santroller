a8d48d8df1c681efdaab77f11cd5c567175803de
Sanjay Govind <sanjay.govind9@gmail.com>
2023-05-16 23:20:18 +1200

    implement buffer based inputs

--- src/shared/controller/controller.h
+++ src/shared/controller/controller.h
@@ -5,7 +5,7 @@
 #define XBOX_AXIS_COUNT 6
 // Arduinos don't have the ram for a queue depth of 1024
 #ifdef __AVR__
-#define BUFFER_SIZE_QUEUE 512 + 128
+#define BUFFER_SIZE_QUEUE 512
 #else
 #define BUFFER_SIZE_QUEUE 1024
 #endif

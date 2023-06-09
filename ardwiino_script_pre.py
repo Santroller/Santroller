1eef8435d9cac87a9dfb4767fda22d5cd5d910f6
Sanjay Govind <sanjay.govind9@gmail.com>
2023-06-09 21:44:10 +1200

    add missing :o

--- ardwiino_script_pre.py
+++ ardwiino_script_pre.py
@@ -99,7 +99,7 @@ for type in ["arduino_mega_2560", "arduino_mega_adk", "arduino_uno"]:
         env.AddCustomTarget(
             name=f"{type}_{proc}_clean",
             dependencies=None,
-            actions=[f'"$PROJECT_PACKAGES_DIR/tool-avrdude/avrdude" -F -C "$PROJECT_PACKAGES_DIR/tool-avrdude/avrdude.conf" -p {board} -c flip1 -U flash:w:"$PROJECT_DIR/../default_firmwares/{type}_usb_{proc}.hex"'],
+            actions=[f'"$PROJECT_PACKAGES_DIR/tool-avrdude/avrdude" -F -C "$PROJECT_PACKAGES_DIR/tool-avrdude/avrdude.conf" -p {board} -c flip1 -U flash:w:"$PROJECT_DIR/../default_firmwares/{type}_usb_{proc}.hex:i"'],
             title=None,
             description=None
         )

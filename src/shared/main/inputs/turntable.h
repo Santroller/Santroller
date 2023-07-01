269995aeb255e1156c26d4ef40c07cfc63af71f8
Sanjay Govind <sanjay.govind9@gmail.com>
2023-07-01 23:12:50 +1200

    fix bluetooth reciver, add configurable turntable poll rate

--- src/shared/main/inputs/turntable.h
+++ src/shared/main/inputs/turntable.h
@@ -1,12 +1,13 @@
 #ifdef INPUT_DJ_TURNTABLE
-lastDj = micros();
 uint8_t *dj_left = lastSuccessfulTurntablePacketLeft;
 uint8_t *dj_right = lastSuccessfulTurntablePacketRight;
 int8_t dj_turntable_left = 0;
 int8_t dj_turntable_right = 0;
-bool djLeftValid = lastTurntableWasSuccessfulLeft;
-bool djRightValid = lastTurntableWasSuccessfulRight;
-if ((micros() - lastDj) > 1000) {
+bool djLeftValid = true;
+bool djRightValid = true;
+bool elapsed = (micros() - lastDj) > INPUT_DJ_TURNTABLE_POLL_RATE;
+if (elapsed) {
+    lastTap = micros();
     djLeftValid = twi_readFromPointer(DJ_TWI_PORT, DJLEFT_ADDR, DJ_BUTTONS_PTR, sizeof(lastSuccessfulTurntablePacketLeft), dj_left);
     djRightValid = twi_readFromPointer(DJ_TWI_PORT, DJRIGHT_ADDR, DJ_BUTTONS_PTR, sizeof(lastSuccessfulTurntablePacketRight), dj_right);
     lastTurntableWasSuccessfulLeft = djLeftValid;
@@ -15,12 +16,14 @@ if ((micros() - lastDj) > 1000) {
 // DJ Hero turntables are pretty noisy, so smooth that out with a moving average
 #ifdef INPUT_DJ_TURNTABLE_SMOOTHING_LEFT
 if (djLeftValid) {
-    dj_sum_left -= dj_last_readings_left[dj_next_left];
-    dj_last_readings_left[dj_next_left] = (int8_t)dj_left[2];
-    dj_sum_left += dj_last_readings_left[dj_next_left];
-    dj_next_left++;
-    if (dj_next_left >= TURNTABLE_BUFFER_SIZE) {
-        dj_next_left = 0;
+    if (elapsed) {
+        dj_sum_left -= dj_last_readings_left[dj_next_left];
+        dj_last_readings_left[dj_next_left] = (int8_t)dj_left[2];
+        dj_sum_left += dj_last_readings_left[dj_next_left];
+        dj_next_left++;
+        if (dj_next_left >= TURNTABLE_BUFFER_SIZE) {
+            dj_next_left = 0;
+        }
     }
     dj_turntable_left = (dj_sum_left / TURNTABLE_BUFFER_SIZE);
 }
@@ -29,12 +32,14 @@ dj_turntable_left = (int8_t)dj_left[2];
 #endif
 #ifdef INPUT_DJ_TURNTABLE_SMOOTHING_RIGHT
 if (djRightValid) {
-    dj_sum_right -= dj_last_readings_right[dj_next_right];
-    dj_last_readings_right[dj_next_right] = (int8_t)dj_right[2];
-    dj_sum_right += dj_last_readings_right[dj_next_right];
-    dj_next_right++;
-    if (dj_next_right >= TURNTABLE_BUFFER_SIZE) {
-        dj_next_right = 0;
+    if (elapsed) {
+        dj_sum_right -= dj_last_readings_right[dj_next_right];
+        dj_last_readings_right[dj_next_right] = (int8_t)dj_right[2];
+        dj_sum_right += dj_last_readings_right[dj_next_right];
+        dj_next_right++;
+        if (dj_next_right >= TURNTABLE_BUFFER_SIZE) {
+            dj_next_right = 0;
+        }
     }
     dj_turntable_right = (dj_sum_right / TURNTABLE_BUFFER_SIZE);
 }

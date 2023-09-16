5e3d5667412646ad4d7173ba1f4b49c852f43245
Sanjay Govind <sanjay.govind9@gmail.com>
2023-09-16 20:08:09 +1200

    Implement neck support for unkown gh5 clone

--- src/shared/main/inputs/clone_neck.h
+++ src/shared/main/inputs/clone_neck.h
@@ -1,5 +1,11 @@
 #ifdef INPUT_CLONE_NECK
+uint8_t clone_data_read[4];
 uint8_t *clone_buttons = lastSuccessfulClonePacket;
-bool cloneValid = twi_writeTo(CLONE_TWI_PORT, CLONE_ADDR, clone_data, sizeof(clone_data), true, true) && twi_readFrom(CLONE_TWI_PORT, CLONE_ADDR, lastSuccessfulClonePacket, sizeof(lastSuccessfulClonePacket), lastSuccessfulClonePacket);
+bool cloneValid = twi_writeTo(CLONE_TWI_PORT, CLONE_ADDR, clone_data, sizeof(clone_data), true, true) && twi_readFrom(CLONE_TWI_PORT, CLONE_ADDR, clone_data_read, sizeof(clone_data_read), true);
 lastCloneWasSuccessful = cloneValid;
+if (clone_data_read[0] != CLONE_VALID_PACKET) {
+    memcpy(clone_data_read, lastSuccessfulClonePacket, sizeof(clone_data_read));
+} else {
+    memcpy(lastSuccessfulClonePacket, clone_data_read, sizeof(clone_data_read));
+}
 #endif

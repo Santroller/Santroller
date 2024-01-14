776b2adca4f4f0138bfb1fdd4ea8cf7669650a64
Sanjay Govind <sanjay.govind9@gmail.com>
2024-01-14 18:36:33 +1300

    Tidy ps2 slider

--- src/shared/main/inputs/ps2.h
+++ src/shared/main/inputs/ps2.h
@@ -6,25 +6,25 @@
     if (ps2Valid) {
         memcpy(lastSuccessfulPS2Packet, ps2Data, sizeof(lastSuccessfulPS2Packet));
         lastTapPS2 = ps2Data[7];
-        if (lastTapPS2 == 0x80) {
+        if (lastTapPS2 > 0x75  && lastTapPS2 < 0x85) {
             lastTapPS2GH5 = 0;
-        } else if (lastTapPS2 > 0x10 && lastTapPS2 < 0x1F) {
+        } else if (lastTapPS2 < 0x1F) {
             lastTapPS2GH5 = 0x95;
-        } else if (lastTapPS2 > 0x30 && lastTapPS2 < 0x3F) {
+        } else if (lastTapPS2 < 0x3F) {
             lastTapPS2GH5 = 0xB0;
-        } else if (lastTapPS2 > 0x50 && lastTapPS2 < 0x5F) {
+        } else if (lastTapPS2 < 0x5F) {
             lastTapPS2GH5 = 0xCD;
-        } else if (lastTapPS2 > 0x60 && lastTapPS2 < 0x6F) {
+        } else if (lastTapPS2 < 0x6F) {
             lastTapPS2GH5 = 0xE6;
-        } else if (lastTapPS2 > 0x90 && lastTapPS2 < 0x9F) {
+        } else if (lastTapPS2 < 0x9F) {
             lastTapPS2GH5 = 0x1A;
-        } else if (lastTapPS2 > 0xA0 && lastTapPS2 < 0xAF) {
+        } else if (lastTapPS2 < 0xAF) {
             lastTapPS2GH5 = 0x2F;
-        } else if (lastTapPS2 > 0xC0 && lastTapPS2 < 0xCF) {
+        } else if (lastTapPS2 < 0xCF) {
             lastTapPS2GH5 = 0x49;
-        } else if (lastTapPS2 > 0xD0 && lastTapPS2 < 0xDF) {
+        } else if (lastTapPS2 < 0xEF) {
             lastTapPS2GH5 = 0x66;
-        } else if (lastTapPS2 == 0xFF) {
+        } else {
             lastTapPS2GH5 = 0x7F;
         }
     }

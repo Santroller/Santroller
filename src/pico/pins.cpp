a0a88db401d05c942a8f31beeaca11fcff100a6f
Sanjay Govind <sanjay.govind9@gmail.com>
2024-03-10 15:31:59 +1300

    Don't poll multiplexer inputs if the tool is polling them

--- src/pico/pins.cpp
+++ src/pico/pins.cpp
@@ -65,7 +65,8 @@ uint16_t adc_read(uint8_t pin, uint8_t mask) {
 uint16_t multiplexer_read(uint8_t pin, uint32_t mask, uint32_t bits) {
     if (!disable_multiplexer) {
         gpio_put_masked(mask, bits);
+        adc_select_input(pin);
+        return adc_read() << 4;
     }
-    adc_select_input(pin);
-    return adc_read() << 4;
+    return 0;
 }

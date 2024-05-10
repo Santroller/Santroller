bc3ec8a20b651c3763dc6411c7359ddca3017cdf
Sanjay Govind <sanjay.govind9@gmail.com>
2024-05-10 15:00:55 +1200

    fix arduino

--- src/avr/shared/usb.cpp
+++ src/avr/shared/usb.cpp
@@ -1,5 +1,6 @@
 #include <stdint.h>
 #include "defines.h"
+#if USB_HOST_STACK
 void send_report_to_controller(uint8_t deviceType, const uint8_t *report, uint8_t len) {
 }
 USB_Device_Type_t get_device_address_for(uint8_t deviceType) {
@@ -8,5 +9,6 @@ USB_Device_Type_t get_device_address_for(uint8_t deviceType) {
 uint8_t transfer_with_usb_controller(const uint8_t dev_addr, const uint8_t requestType, const uint8_t request, const uint16_t wValue, const uint16_t wIndex, const uint16_t wLength, uint8_t *buffer) {
     return 0;
 }
+#endif
 void authentication_successful() {
 }

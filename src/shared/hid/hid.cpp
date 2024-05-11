efef0dd23253a6a01465fe1f22da2384743b9fa1
Sanjay Govind <sanjay.govind9@gmail.com>
2024-05-11 15:12:47 +1200

    Fix rumble for gamepads

--- src/shared/hid/hid.cpp
+++ src/shared/hid/hid.cpp
@@ -345,7 +345,7 @@ void handle_rumble(uint8_t rumble_left, uint8_t rumble_right) {
     }
 #endif
 
-#if defined(INPUT_USB_HOST) && DEVICE_TYPE_IS_NORMAL_GAMEPAD
+#if defined(INPUT_USB_HOST) && DEVICE_TYPE == GAMEPAD
     USB_Device_Type_t type;
     for (uint8_t i = 0; i < get_usb_host_device_count(); i++) {
         type = get_usb_host_device_type(i);

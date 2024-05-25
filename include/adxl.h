3aab26cc67922e73173f8364581abf060b0121d9
Sanjay Govind <sanjay.govind9@gmail.com>
2024-05-25 20:28:30 +1200

    move adxl low pass alpha to config_data

--- include/adxl.h
+++ include/adxl.h
@@ -7,7 +7,6 @@ extern "C" {
 #define ADXL345_DATA_FORMAT 0x31
 #define ADXL345_DATAX0 0x32
 #define ADXL345_GRAVITY_EARTH        9.80665f
-#define LOW_PASS_ALPHA 0.5
 void tick_adxl();
 void init_adxl();
 #ifdef __cplusplus

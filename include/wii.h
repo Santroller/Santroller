c9c137a848deb7d37181b28eb96004be7877a402
Sanjay Govind <sanjay.govind9@gmail.com>
2024-05-30 13:01:21 +1200

    Nkyo frontman support (and other wii peripherals that need encryption)

--- include/wii.h
+++ include/wii.h
@@ -4,6 +4,10 @@ extern "C" {
 #endif
 #define WII_ADDR 0x52
 #define WII_READ_ID 0xFA
+#define WII_ENCRYPTION_ID 0xF0
+#define WII_ENCRYPTION_KEY_ID 0x40
+#define WII_ENCRYPTION_KEY_ID_2 0x46
+#define WII_ENCRYPTION_KEY_ID_3 0x4C
 #define WII_ID_LEN 6
 #define WII_DJ_EUPHORIA 0xFB
 #define WII_SET_RES_MODE 0xFE
--- src/shared/main/wii.cpp
+++ src/shared/main/wii.cpp
@@ -11,6 +11,7 @@ uint8_t wiiBytes;
 #ifdef INPUT_WII
 uint8_t wiiPointer = 0;
 bool hiRes = false;
+bool encrypted = false;
 bool verifyData(const uint8_t* dataIn, uint8_t dataSize) {
     uint8_t orCheck = 0x00;   // Check if data is zeroed (bad connection)
     uint8_t andCheck = 0xFF;  // Check if data is maxed (bad init)
@@ -59,6 +60,7 @@ void initWiiExt(void) {
     wiiPointer = 0;
     wiiBytes = 6;
     hiRes = false;
+    encrypted = false;
     if (wiiControllerType == WII_CLASSIC_CONTROLLER ||
         wiiControllerType == WII_CLASSIC_CONTROLLER_PRO) {
         // Enable high-res mode (try a few times, sometimes the controller doesnt
@@ -86,7 +88,31 @@ void initWiiExt(void) {
         // get read back are constant. Hence we start at 0x5 instead of 0x0.
         wiiPointer = 5;
         wiiBytes = 1;
+    } 
+    delayMicroseconds(200);
+    uint8_t data[8] = {0};
+    twi_readFromPointerSlow(WII_TWI_PORT, WII_ADDR, wiiPointer, wiiBytes, data);
+    uint8_t orCheck = 0x00;   
+    for (int i = 0; i < wiiBytes; i++) {
+        orCheck |= data[i];
     }
+    // Controller requires encryption (nyko frontman)
+    if (orCheck == 0) {
+        delayMicroseconds(200);
+        // Enable encryption
+        twi_writeSingleToPointer(WII_TWI_PORT, WII_ADDR, WII_ENCRYPTION_ID, 0xAA);
+        delayMicroseconds(200);
+        // Write zeroed key in blocks
+        uint8_t key[6] = {0};
+        twi_writeToPointer(WII_TWI_PORT, WII_ADDR, WII_ENCRYPTION_KEY_ID, 6, key);
+        delayMicroseconds(200);
+        twi_writeToPointer(WII_TWI_PORT, WII_ADDR, WII_ENCRYPTION_KEY_ID_2, 6, key);
+        delayMicroseconds(200);
+        twi_writeToPointer(WII_TWI_PORT, WII_ADDR, WII_ENCRYPTION_KEY_ID_3, 4, key);
+        delayMicroseconds(200);
+        encrypted = true;
+    }
+    
 }
 bool initialised = false;
 bool lastWiiEuphoriaLed = false;
@@ -103,6 +129,24 @@ uint8_t* tickWii() {
         initialised = false;
         initWiiExt();
         return NULL;
+    }
+    if (encrypted) {
+        // https://github.com/TheNathannator/WiitarThing/commit/1b81d187dbedaa7bec43c5e38bc17f6eedffaa6a
+        // Nyko frontman requires using a different decryption transform seed
+        uint8_t decrypted = (uint8_t)(((data[4] ^ 0x97) + 0x97) & 0xFF);
+        bool useCommonValue = (data[4] != 0xFF) && ((decrypted & 0xAB) == 0xAB);
+
+        for (int i = 0; i < 6; i++)
+        {
+            if (useCommonValue)
+            {
+                data[i] = (uint8_t)(((data[i] ^ 0x97) + 0x97) & 0xFF);
+            }
+            else
+            {
+                data[i] = (uint8_t)(((data[i] ^ 0x4D) + 0x4D) & 0xFF);
+            }
+        }
     }
 #if DEVICE_TYPE == DJ_HERO_TURNTABLE
     // Update the led if it changes

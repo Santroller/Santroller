ce1ebc1aacc83e6121d4cf67d14b6b05b9eba7fc
Sanjay Govind <sanjay.govind9@gmail.com>
2024-06-01 18:27:26 +1200

    Rewrite slider bar logic, set flags for xinput guitars so that slider works on 360

--- src/shared/main/inputs/ps2.h
+++ src/shared/main/inputs/ps2.h
@@ -7,25 +7,25 @@
         memcpy(lastSuccessfulPS2Packet, ps2Data, sizeof(lastSuccessfulPS2Packet));
         lastTapPS2 = ps2Data[7];
         if (lastTapPS2 < 0x2F) {
-            lastTapPS2GH5 = 0x95;
+            lastTapPS2GH5 = 0x15;
         } else if (lastTapPS2 < 0x3F) {
-            lastTapPS2GH5 = 0xB0;
+            lastTapPS2GH5 = 0x30;
         } else if (lastTapPS2 < 0x5F) {
-            lastTapPS2GH5 = 0xCD;
+            lastTapPS2GH5 = 0x4D;
         } else if (lastTapPS2 < 0x6F) {
-            lastTapPS2GH5 = 0xE6;
-        } else if (lastTapPS2 < 0x8F) {
-            lastTapPS2GH5 = 0;
-        } else if (lastTapPS2 < 0x9F) {
-            lastTapPS2GH5 = 0x1A;
-        } else if (lastTapPS2 < 0xAF) {
-            lastTapPS2GH5 = 0x2F;
-        } else if (lastTapPS2 < 0xCF) {
-            lastTapPS2GH5 = 0x49;
-        } else if (lastTapPS2 < 0xEF) {
             lastTapPS2GH5 = 0x66;
+        } else if (lastTapPS2 < 0x8F) {
+            lastTapPS2GH5 = 0x80;
+        } else if (lastTapPS2 < 0x9F) {
+            lastTapPS2GH5 = 0x9A;
+        } else if (lastTapPS2 < 0xAF) {
+            lastTapPS2GH5 = 0xAF;
+        } else if (lastTapPS2 < 0xCF) {
+            lastTapPS2GH5 = 0xC9;
+        } else if (lastTapPS2 < 0xEF) {
+            lastTapPS2GH5 = 0xE6;
         } else {
-            lastTapPS2GH5 = 0x7F;
+            lastTapPS2GH5 = 0xFF;
         }
     }
     

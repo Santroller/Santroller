57535401ee8674ca60b88f3c91d295f861b50b32
Sanjay Govind <sanjay.govind9@gmail.com>
2025-05-04 21:49:31 +1200

    fix bh drums

--- src/shared/main/bh_drum.cpp
+++ src/shared/main/bh_drum.cpp
@@ -7,22 +7,24 @@
 #define BH_DRUM_PTR 0x10
 #ifdef BH_DRUM_TWI_PORT
 bool bh_drum_found = false;
+static long lastTick = 0;
+static int missing = 0;
 void tickBhDrum() {
-    // BH Drum format: 0x10 <midi packet>
-    uint8_t data[8];
-    bh_drum_found = twi_readFromPointer(BH_DRUM_TWI_PORT, DRUM_ADDR, BH_DRUM_PTR, sizeof(data), data) && data[0] == BH_DRUM_PTR;
+    // BH Drum format: 0x10 0x<packetsinbuffer>0 <midi packet (of len 3, if you request more bytes it will just return multiple packets)>
+    uint8_t data[5];
+    bh_drum_found = twi_readFromPointer(BH_DRUM_TWI_PORT, DRUM_ADDR, BH_DRUM_PTR, sizeof(data), data);
     if (bh_drum_found) {
-        uint8_t status = data[1];
+        uint8_t status = data[2];
         uint8_t type = (status & 0xf0);
         uint8_t channel = status & 0x0f;
         // TODO: CC and stuff, can the midi lib parse this all for us?
         if (type == 0x90) {
-            uint8_t note = data[2];
-            uint8_t velocity = data[3];
+            uint8_t note = data[3];
+            uint8_t velocity = data[4];
             onNote(channel, note, velocity);
         } else if (type == 0x80) {
-            uint8_t note = data[2];
-            uint8_t velocity = data[3];
+            uint8_t note = data[3];
+            uint8_t velocity = data[4];
             offNote(channel, note, velocity);
         }
     }

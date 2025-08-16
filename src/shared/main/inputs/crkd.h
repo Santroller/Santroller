33bd5754909d17f8205316133f8cab10c4e72d93
Sanjay Govind <sanjay.govind9@gmail.com>
2025-08-16 23:24:14 +1200

    crkd neck support

--- src/shared/main/inputs/crkd.h
+++ src/shared/main/inputs/crkd.h
@@ -1,3 +1,3 @@
 #ifdef INPUT_CRKD_NECK
-    lastCrkdWasSuccessful = read_uart(CRKD_UART_PORT, 0x65, 11, (uint8_t*)&lastCrkd);
+    lastCrkdWasSuccessful = read_uart(CRKD_UART_PORT, 0xA5, 11, (uint8_t*)&lastCrkd);
 #endif

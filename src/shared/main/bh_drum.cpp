5d9ab89f55162c1f8522c38d2e1cdc7c533a3498
Sanjay Govind <sanjay.govind9@gmail.com>
2025-12-10 22:08:28 +1300

    better bh drums

--- src/shared/main/bh_drum.cpp
+++ src/shared/main/bh_drum.cpp
@@ -11,30 +11,26 @@ bool bh_drum_found = false;
 void tickBhDrum()
 {
     // Drum packet starts with some counter, and then the number of packets in the buffer
-    uint8_t data[2];
-    bh_drum_found = twi_readFromPointer(BH_DRUM_TWI_PORT, DRUM_ADDR, BH_DRUM_PTR, sizeof(data), data);
+    uint8_t header[2];
+    uint8_t data[3];
+    bh_drum_found = twi_readFromPointer(BH_DRUM_TWI_PORT, DRUM_ADDR, BH_DRUM_PTR, sizeof(header), header);
     if (bh_drum_found)
     {
         // Stream out the rest of the packets byte by byte as the drums lock up otherwise.
-        uint8_t count = data[1] >> 4;
+        uint8_t count = header[1] >> 4;
         for (int i = 0; i < count; i++)
         {
-            uint8_t status;
-            uint8_t note;
-            uint8_t velocity;
-            bh_drum_found = twi_readFrom(BH_DRUM_TWI_PORT, DRUM_ADDR, &status, 1, 1);
-            bh_drum_found = twi_readFrom(BH_DRUM_TWI_PORT, DRUM_ADDR, &note, 1, 1);
-            bh_drum_found = twi_readFrom(BH_DRUM_TWI_PORT, DRUM_ADDR, &velocity, 1, 1);
-            uint8_t type = (status & 0xf0);
-            uint8_t channel = status & 0x0f;
+            bh_drum_found = twi_readFrom(BH_DRUM_TWI_PORT, DRUM_ADDR, data, sizeof(data), true);
+            uint8_t type = (data[0] & 0xf0);
+            uint8_t channel = data[0] & 0x0f;
             // TODO: CC and stuff, can the midi lib parse this all for us?
             if (type == 0x90)
             {
-                TRANSLATE_GH_MIDI(channel, note, velocity, onNote);
+                TRANSLATE_GH_MIDI(channel, data[1], data[2], onNote);
             }
             else if (type == 0x80)
             {
-                TRANSLATE_GH_MIDI(channel, note, velocity, offNote);
+                TRANSLATE_GH_MIDI(channel, data[1], data[2], offNote);
             }
         }
     }

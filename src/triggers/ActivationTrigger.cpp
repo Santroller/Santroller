3ca8fe0e1a68d920850bc4fbef1499002d0bd3d2
Sanjay Govind <sanjay.govind9@gmail.com>
2026-03-11 14:51:55 +1300

    if mapping is incomplete, dont use it as assignment

--- src/triggers/ActivationTrigger.cpp
+++ src/triggers/ActivationTrigger.cpp
@@ -11,17 +11,15 @@ bool ActivationTriggerList::validate(bool claim_devices, bool full_poll, bool se
     if (claim_devices && m_claimed) {
         return true;
     }
-    bool seen = false;
+    if (triggers.empty()) {
+        return false;
+    }
     for (auto &trigger : triggers)
     {
-        seen = true;
         if (!trigger->validate(false, full_poll, send_events))
         {
             return false;
         }
-    }
-    if (!seen) {
-        return false;
     }
     if (!claim_devices)
     {

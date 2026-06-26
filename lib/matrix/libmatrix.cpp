1a50b3565151b6113c38146c641a634d7522c636
Sanjay Govind <sanjay.govind9@gmail.com>
2026-06-26 15:18:37 +0800

    explcitly update matrix out pins, so they can be shared with other inputs

--- lib/matrix/libmatrix.cpp
+++ lib/matrix/libmatrix.cpp
@@ -6,9 +6,11 @@ Matrix::Matrix(uint32_t pins, uint32_t outPins): m_pins(pins), m_outPins(outPins
     gpio_set_dir_out_masked(outPins);
 }
 bool Matrix::read(uint32_t outPin, uint8_t in_pin) {
+    gpio_set_dir_out_masked(m_outPins);
     gpio_put_masked(m_outPins, 1 << outPin);
     sleep_us(1);
     uint8_t ret = gpio_get(in_pin);
     gpio_put_masked(m_outPins, 0);
+    gpio_set_dir_in_masked(m_outPins);
     return ret;
 }
